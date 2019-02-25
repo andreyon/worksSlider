e43a52b8ecccafca0a5ec11ece8a798d70b69614
Andrey Miroshnichenko <miron152008@gmail.com>
2019-02-25 19:35:15 +0200

    init

---
+++ main.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function () {
+    /*$('.slider-container').slick({
+        centerMode: true,
+//центруем текущий слайд
+        centerPadding: '50px',
+//слегка увеличиваем текущий слайд
+        slidesToShow: 3
+    });*/
+
+
+    $('.slider-container').slick({
+        centerMode: true,
+//центруем текущий слайд
+        centerPadding: '50px',
+//слегка увеличиваем текущий слайд
+        slidesToShow: 3
+    });
+});
